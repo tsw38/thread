9f58db232d75a3c744b21871c07fec5c70f2a450
Tyler Williams <tyler.scott.14@gmail.com>
2020-09-19 19:25:17 -0500

    too much pollution

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -17,21 +17,25 @@ class MyDocument extends Document {
             <Html>
                 <Head>
                     <link rel="icon" href="/favicon.svg" />
-                    <script
-                        async
-                        src="https://www.googletagmanager.com/gtag/js?id=G-W7FRMBJZCQ"
-                    />
-                    <script
-                        dangerouslySetInnerHTML={{
-                            __html: `
-                                window.dataLayer = window.dataLayer || [];
-                                function gtag(){dataLayer.push(arguments);}
-                                gtag('js', new Date());
+                    {process.env.ENV !== 'dev' && (
+                        <>
+                            <script
+                                async
+                                src="https://www.googletagmanager.com/gtag/js?id=G-W7FRMBJZCQ"
+                            />
+                            <script
+                                dangerouslySetInnerHTML={{
+                                    __html: `
+                                    window.dataLayer = window.dataLayer || [];
+                                    function gtag(){dataLayer.push(arguments);}
+                                    gtag('js', new Date());
 
-                                gtag('config', 'G-W7FRMBJZCQ');
-                            `,
-                        }}
-                    />
+                                    gtag('config', 'G-W7FRMBJZCQ');
+                                `,
+                                }}
+                            />
+                        </>
+                    )}
                 </Head>
                 <body>
                     <Main />
